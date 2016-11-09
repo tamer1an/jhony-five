26ccd3f76f0ea85b12dfbada20f6898c568d35c3
Trybynenko Andrii <trybynenko@gmail.com>
2016-11-10 00:40:50 +0100

    lcd with buttons

--- src/lcd.js
+++ src/lcd.js
@@ -1,66 +1,30 @@
-var five = require("johnny-five"),
-  board, lcd;
-
-board = new five.Board();  // var board = new five.Board({ port: "COM5" });
+let five = require("johnny-five"),
+  board = new five.Board();  // { port: "COM5" };
 
 board.on("ready", function() {
-  var btnRIGHT = new five.Button("A0"), // 0,
-      btnUP    = new five.Button("A1"), // 1,
-      btnDOWN  = new five.Button("A2"), // 2,
-      btnLEFT  = new five.Button("A3"), // 3,
-      btnSELECT= new five.Button("A4"), // 4,
-      btnNONE  = new five.Button("A5"); // 5;
+  let btnRIGHT = new five.Button("A0"), // 0,
+    // btnUP    = new five.Button("A1"), // 1,
+    // btnDOWN  = new five.Button("A2"), // 2,
+    // btnLEFT  = new five.Button("A3"), // 3,
+    // btnSELECT= new five.Button("A4"), // 4,
+    // btnNONE  = new five.Button("A5"), // 5;
 
-  // new five.Buttons([ 2, 3, 4, 5 ]);
-  // new five.Buttons([ { pin: 2 }, { pin: 3 }, { pin: 4 }, { pin: 5 } ]);
-  // new five.Buttons([ button1, button2, button3 ]);
+    lcd = setupLCD(new five.LCD({
+      // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+      pins: [8, 9, 4, 5, 6, 7],
+      backlight: 10,
+      rows: 2,
+      cols: 20
+      /*Options:
+       bitMode: 4 or 8, defaults to 4
+       lines: number of lines, defaults to 2
+       dots: matrix dimensions, defaults to "5x8"*/
+    }));
 
-  lcd = new five.LCD({
-    // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
-    pins: [8, 9, 4, 5, 6, 7],
-    backlight: 10,
-    rows: 2,
-    cols: 20
+  bindLCDButton(btnRIGHT);
 
-    // Options:
-    // bitMode: 4 or 8, defaults to 4
-    // lines: number of lines, defaults to 2
-    // dots: matrix dimensions, defaults to "5x8"
-  });
-
-  // Tell the LCD you will use these characters:
-  lcd.useChar("check");
-  lcd.useChar("heart");
-  lcd.useChar("duck");
-
-  lcd.clear().print("Hello Juliia");
-
-  // this.wait(3000, function() {
-  //   lcd.clear().cursor(0, 0).print("Happy birthday!!!");
-  //   lcd.print("I").write(7).print(":heart: :heart: :heart: you");
-  // });
-
-  btnUP.on("hold", function() {
-    lcd.clear().cursor(0, 0).print("Happy birthday!!!");
-  });
-
-  btnUP.on("press", function() {
-    lcd.clear().cursor(0, 0).print("Happy birthday!!!");
-  });
-
-  btnUP.on("release", function() {
-    lcd.clear().cursor(0, 0).print("Happy birthday!!!");
-  });
-
-  btnDOWN.on("hold", function() {
-    lcd.print("I").write(7).print(":heart: :heart: :heart: you");
-  });
-
-  btnDOWN.on("press", function() {
-    lcd.print("I").write(7).print(":heart: :heart: :heart: you");
-  });
-
-  btnDOWN.on("release", function() {
+  this.wait(3000, function () {
+    lcd.clear().cursor(0, 0).print("Move cursor");
     lcd.print("I").write(7).print(":heart: :heart: :heart: you");
   });
 
@@ -68,6 +32,35 @@ board.on("ready", function() {
     lcd: lcd
   });
 });
+
+function setupLCD(lcd) {
+  // Tell the LCD you will use these characters:
+  lcd.useChar("check");
+  lcd.useChar("heart");
+  lcd.useChar("duck");
+
+  lcd.clear().print("Hello Board");
+
+  return lcd;
+}
+
+function bindLCDButton(btn) {
+  // btn.on("hold", function() {
+  //   console.log('wait');
+  // });
+
+  btn.on("press", function () {
+    console.log('pressed');
+  });
+
+  btn.on("release", function () {
+    console.log('press and hold');
+  });
+
+  /*  new five.Buttons([ 2, 3, 4, 5 ]);
+   new five.Buttons([ { pin: 2 }, { pin: 3 }, { pin: 4 }, { pin: 5 } ]);
+   new five.Buttons([ button1, button2, button3 ]);  */
+}
 
 /*
   Pin	Function
